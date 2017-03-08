65203b2b7cc772aac3f31b633f56c11bb77bbb3b
Brian Flannery <bflannery66@gmail.com>
2017-03-08 16:55:42 -0600

    main header logo a link

--- app/scripts/Components/Header.js
+++ app/scripts/Components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeaderNav from './HeaderNav';
+import {Link} from 'react-router';
+import {browserHistory} from 'react-router';
 
 export default React.createClass({
   render() {
     return (
       <div className="header">
         <div className="header-logo-container">
-          <h2 className="header-logo"> We.Moxie </h2>
+          <h2 className="header-logo">
+            <Link to="/home">We.Moxie </Link>
+          </h2>
         </div>
         <HeaderNav />
         </div>
