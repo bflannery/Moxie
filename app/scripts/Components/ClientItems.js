5d56519f2a6d42b72b2ee41a26e678ece9e6395c
Brian Flannery <bflannery66@gmail.com>
2017-04-22 11:13:10 -0500

    merging sortItems

--- app/scripts/Components/ClientItems.js
+++ app/scripts/Components/ClientItems.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import ClientItem from './ClientItem';
 
-import store from '../store';
-
 export default React.createClass({
   render() {
-    console.log(this.props);
-    let allItems;
-    let clientItems = [];
-    let client = this.props.client;
+  let allItems = [];
+    let folders = this.props.client.clientFolders;
+    let files = this.props.client.clientFiles;
+    allItems = allItems.concat(files, folders);
 
-
-    if(!client.clientFiles) {
-      allItems = <div />;
-    } else {
-       clientItems = clientItems.concat(client.clientFiles, client.clientFolders);
-       console.log(clientItems);
-
-    allItems = clientItems.map((clientItem, arr, i ) => {
-      return <ClientItem key={i} clientItem={clientItem} session={this.props.session}/>
+    let allClientItems =  allItems.map((item , i, arr)=> {
+      return <ClientItem key={i} item={item} session={this.props.session}/>
     });
-  }
 
     return (
-      <ul className ="secondary-container">
-        {allItems}
-      </ul>
-    );
-  }
+      <div className="all-client-items">
+      {allClientItems}
+      </div>
 });
